49f69660cc646136bdb607264453bc649af867d5
codepo8 <chris.heilmann@gmail.com>
2020-10-21 09:01:37 +0200

    added autocomplete, CSS fixed images

--- walkies.js
+++ walkies.js
@@ -1,31 +1,35 @@
 let breeds = {};
 let url = 'https://dog.ceo/api/breeds/image/random';
-let breedsselector = document.querySelector('#breed');
-let subselector = document.querySelector('#sub');
-let subsection = document.querySelector('#subsection');
+let breed = document.querySelector('#breed');
+let imagecontainer = document.querySelector('button div');
 let breedname = document.querySelector('button span');
 let button = document.querySelector('button');
 
 const findbreed = _ => {
-  let name = breedsselector.value;
+  let name = breed.value;
   breedname.innerHTML = name;
-  name = name.replace(' - ','/');
+  name = name.replace(' - ', '/').toLowerCase();
   url = `https://dog.ceo/api/breed/${name}/images/random`;
-  getdog();
+  getdog(); 
 };
 
 const seedbreedsform = _ => {
   let out = '';
   Object.keys(breeds).forEach(b => {
-    out += `<option value="${b}">${b}</option>`;
-    if(breeds[b].length > 0) {
+    out += `<option value="${ucfirst(b)}">${ucfirst(b)}</option>`;
+    if (breeds[b].length > 0) {
       breeds[b].forEach(s => {
-        out += `<option value="${b} - ${s}">${b} - ${s}</option>`;
+        out += `<option value="${ucfirst(b)} - ${ucfirst(s)}">${ucfirst(b)} - ${ucfirst(s)}</option>`;
       });
     }
   });
   document.querySelector('#allbreeds').innerHTML = out;
 };
+
+const ucfirst = str => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 
 const getbreeds = _ => {
   fetch('https://dog.ceo/api/breeds/list/all')
@@ -41,12 +45,12 @@ const getdog = _ => {
   fetch(url)
     .then(response => response.json())
       .then(data => {
-        document.querySelector('img').src = data.message;
+        imagecontainer.style.backgroundImage = `url(${data.message})`;
         button.classList.remove('loading');
   });
 };
 
-breedsselector.addEventListener('change', findbreed);
+breed.addEventListener('change', findbreed);
 button.addEventListener('click', getdog);
 getdog();
 getbreeds();
